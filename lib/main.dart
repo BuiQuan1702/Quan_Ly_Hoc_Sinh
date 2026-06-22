66ffd9cac7ae7846c33b7d381cd061394b8e938a
BuiQuan1702 <buiquan653@gmail.com>
2026-06-22 07:17:51 +0700

    hỗ trợ đăng ký nhiều lớp, tính điểm theo trọng số và tối ưu giao diện sinh viên

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'screens/login_screen.dart';
-
+// test nạp codeeeeee
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
